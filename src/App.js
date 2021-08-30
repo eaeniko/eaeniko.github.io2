5714df097a1ecc65ecd6bc3c149be06aed984822
eaeniko <eaenikolai@gmail.com>
2021-08-30 09:01:24 -0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This <code>site</code> is under development.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wait.
         </a>
       </header>
     </div>
