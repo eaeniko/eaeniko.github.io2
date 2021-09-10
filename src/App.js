8ebce0b08839bb0d321f3222dca3e5b34ba90b5f
eaeniko <eaenikolai@gmail.com>
2021-09-10 15:39:27 -0300

    first test using react-boostrap, update link to button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -9,14 +11,10 @@ function App() {
         <p>
          This <code>site</code> is under development By Nikolai Cinotti.
         </p>
-        <a
-          className="App-link"
+        <Button 
           href="https://bit.ly/eaeniko"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check my others links!
-        </a>
+          rel="noopener noreferrer" > Check my others links! </Button>
       </header>
     </div>
   );
